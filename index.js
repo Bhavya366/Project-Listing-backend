1b161a1aeb6ca2df33f5381a239f0e56ef86426f
Bhavya366 <21b01a12c8@svecw.edu.in>
2023-06-23 23:13:01 +0530

    updated

--- index.js
+++ index.js
@@ -14,12 +14,8 @@ const Product = require('./models/productModel')
 dotenv.config();
 
 // middleware
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'))
